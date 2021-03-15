b9f066623e0f1bba5219df257b1b735dc7e69490
OmairLiaquatAli <omair.ali@evan.team>
2021-03-15 14:34:12 +0100

    add integration test

--- tests/workflows.rs
+++ tests/workflows.rs
@@ -1,16 +1,89 @@
 use vade_evan_bbs::application::{issuer::Issuer, prover::Prover};
-
+use vade_evan_bbs::application::datatypes::{
+    CredentialSchema, CREDENTIAL_PROPOSAL_TYPE,
+    CREDENTIAL_REQUEST_TYPE, CREDENTIAL_OFFER_TYPE,
+};
+use bbs::{
+    keys::{ DeterministicPublicKey, SecretKey }, SignatureBlinding,
+    SignatureMessage,
+};
+use std::collections::HashMap;
+use std::error::Error;
+use utilities::test_data::{
+    accounts::local::{ HOLDER_DID, ISSUER_DID},
+    bbs_coherent_context_test_data::{
+        MASTER_SECRET, NQUADS, PUB_KEY, SIGNATURE_BLINDING, SECRET_KEY,
+    },
+    vc_zkp::{ EXAMPLE_CREDENTIAL_SCHEMA}};
 #[test]
-fn test_issuance_workflow() {
+fn test_issuance_workflow() -> Result<(), Box<dyn Error>>{
     // Create credential proposal
-    let proposal = Prover::propose_credential(/*arguments*/);
-
+    let proposal = Prover::propose_credential(&ISSUER_DID, &HOLDER_DID, "schemadid");
+    assert_eq!(&proposal.subject, &HOLDER_DID);
+    assert_eq!(&proposal.issuer, &ISSUER_DID);
+    assert_eq!(&proposal.schema, "schemadid");
+    assert_eq!(&proposal.r#type, CREDENTIAL_PROPOSAL_TYPE);
+    
     // Create credential offering
-    let offering = Issuer::offer_credential(/*arguments*/);
+    let offering = Issuer::offer_credential(&proposal, &ISSUER_DID)?;
+    assert_eq!(&offering.issuer, &ISSUER_DID);
+    assert_eq!(&offering.schema, &proposal.schema);
+    assert_eq!(&offering.subject, &proposal.subject);
+    assert_eq!(&offering.r#type, &CREDENTIAL_OFFER_TYPE);
 
     // Create credential request
+    let mut credential_values = HashMap::new();
+    credential_values.insert("test_property_string".to_owned(), "value".to_owned());
+    
+    let schema: CredentialSchema = serde_json::from_str(EXAMPLE_CREDENTIAL_SCHEMA)?;
+
+    let master_secret: SignatureMessage =
+    SignatureMessage::from(base64::decode(&MASTER_SECRET)?.into_boxed_slice());
+
+    let public_key: DeterministicPublicKey =
+    DeterministicPublicKey::from(base64::decode(&PUB_KEY)?.into_boxed_slice());
+
+    let (credential_request, _) = Prover::request_credential(&offering, &schema, &master_secret, credential_values, &public_key)
+    .map_err(|e| format!("{}", e))?;
+    assert_eq!(credential_request.schema, schema.id);
+    assert_eq!(credential_request.subject, offering.subject);
+    assert_eq!(credential_request.r#type, CREDENTIAL_REQUEST_TYPE);
 
     // Issue credential
-
+    let key_id = format!("{}#key-1", ISSUER_DID);
+    let sk: SecretKey = SecretKey::from(base64::decode(&SECRET_KEY)?.into_boxed_slice());
+    let nquads = Vec::new();
+    let unfinished_credential = Issuer::issue_credential(
+        &ISSUER_DID,
+        &HOLDER_DID,
+        &offering,
+        &credential_request,
+        &key_id,
+        &public_key,
+        &sk,
+        schema.clone(),
+        [1].to_vec(),
+        nquads,
+    )?;
+    
     // Finish credential
+    let nquads: Vec<String> = NQUADS.iter().map(|q| q.to_string()).collect();
+    let blinding: SignatureBlinding =
+    SignatureBlinding::from(base64::decode(&SIGNATURE_BLINDING)?.into_boxed_slice());
+    match Prover::finish_credential(
+        &unfinished_credential,
+        &master_secret,
+        &nquads,
+        &public_key,
+        &blinding,
+    ) {
+        Ok(cred) => {
+            // There is now a property 'signature' and it is base64 encoded
+            assert!(base64::decode(&cred.proof.signature).is_ok());
+        }
+        Err(e) => {
+            assert!(false, "Unexpected error when finishing credential: {}", e);
+        }
+    }
+    Ok(())
 }
